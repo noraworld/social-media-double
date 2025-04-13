ba3e2ed26add1ba052a3237b45115509fa3d090f
Kosuke Aoki <mail@noraworld.com>
2025-04-13 18:05:34 +0900

    Add ability to post attached files
    It took ages like crazy to implement this...

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict'
 
+import crypto from 'crypto'
 import * as Misskey from 'misskey-js';
 import { Octokit } from '@octokit/rest';
 
+const cache = new Map();
 const spiltCommentsCheckAttemptsMaximum = process.env.DRY_RUN === 'true' ? 1 : 5;
 
 async function run() {
@@ -50,9 +52,15 @@ async function getComments() {
 }
 
 async function post(comments) {
+  let content;
+  let fileIDs;
+
   for (let comment of comments) {
     try {
-      await createNote(comment.body);
+      content = await extractAttachedFiles(comment.body);
+      fileIDs = await uploadFiles(content.files);
+
+      await createNote(content.body, fileIDs);
       await deleteComment(comment.id);
     }
     catch (error) {
@@ -63,29 +71,54 @@ async function post(comments) {
   }
 }
 
-async function createNote(commentBody) {
+async function createNote(contentBody, fileIDs) {
+  const params = {
+    visibility: 'public', // TODO: add ability to specify this with a specific expression in the comment
+    cw: null,             // TODO: add ability to specify this with a specific expression in the comment
+    text: contentBody
+  }
+
+  // { fileIds: [] } or { fileIds: null } is not acceptable
+  if (fileIDs.length) params.fileIds = fileIDs;
+
   if (process.env.DRY_RUN === 'true') {
-    console.info(`The comment "${commentBody.split(/[\r\n|\r|\n]/)[0]}" is supposed to be posted, but not done because dry run is enabled.`);
+    console.info(`The comment "${contentBody.split(/[\r\n|\r|\n]/)[0]}" is supposed to be posted, but not done because dry run is enabled.`);
+    console.info('params: ', params);
     return true;
   }
 
-  const note = await setupAPI().misskey.request(
-    'notes/create',
-    {
-      visibility: 'public', // TODO: add ability to specify this with a specific expression in the comment
-      cw: null,             // TODO: add ability to specify this with a specific expression in the comment
-      text: commentBody
-    }
-  );
+  const note = await setupAPI().misskey.request('notes/create', params);
 
   return note;
+}
+
+// INFO: it may upload the same files when the comment is different, even if the caches are found
+async function uploadFiles(files) {
+  if (!files.length) return files;
+  const fileIDs = [];
+
+  for (let file of files) {
+    if (process.env.DRY_RUN === 'true') {
+      console.info(`The file <Buffer ${file.length} bytes> is supposed to be uploaded, but not done because dry run is enabled.`);
+      fileIDs.push(Math.random().toString(32).substring(2));
+      continue;
+    }
+
+    // misskey-js doesn't work properly
+    // https://github.com/noraworld/to-do/issues/1437
+    const response = await callAPIbyFetch('drive/files/create', file);
+    fileIDs.push(response.id);
+    console.log(`fileIDs: ${fileIDs}`);
+  }
+
+  return fileIDs;
 }
 
 async function deleteComment(commentID) {
   const api = setupAPI()
 
   if (process.env.DRY_RUN === 'true') {
-    console.info(`The comment "${commentID}" is supposed to be deleted, but not done because dry run is enabled.`);
+    console.info(`The comment "${commentID}" is supposed to be deleted, but not done because dry run is enabled.\n`);
     return true;
   }
 
@@ -94,6 +127,22 @@ async function deleteComment(commentID) {
     repo: api.repo,
     comment_id: commentID,
   })
+}
+
+async function callAPIbyFetch(endpoint, file) {
+  const form = new FormData();
+  form.append('i', process.env.MISSKEY_API_TOKEN);
+  form.append('file', new Blob([file.buffer]), file.name);
+
+  const response = await fetch(
+    `https://${process.env.MISSKEY_SERVER}/api/${endpoint}`,
+    {
+      method: 'POST',
+      body: form
+    }
+  );
+
+  return await response.json();
 }
 
 function setupAPI() {
@@ -114,6 +163,89 @@ function setupAPI() {
     repo: repo,
     misskey: misskey
   }
+}
+
+// https://chatgpt.com/share/67a6fe0a-c510-8004-9ed8-7b106493bb4a
+// https://chatgpt.com/share/67dc00c4-4b0c-8004-9e30-4cd77023249a
+// https://chatgpt.com/share/67fa6146-f6c4-8004-9c22-3891c4884d85
+// https://www.bugbugnow.net/2020/02/Escape-characters-used-in-regular-expressions.html
+async function extractAttachedFiles(commentBody) {
+  // a simple way to detect links like ![foo](https://example.com) and ignore `![foo](https://example.com)` at the same time
+  // but not perfect because it doesn't ignore the case like `hello ![foo](https://example.com) world`
+  const regex = /(?<!`)(?:!\[.*?\]\((https?:\/\/[^\s)]+)\)|<img.*?src="(https?:\/\/[^\s"]+)"(?!.*exclude).*>)(?!`)/g;
+  const files = [];
+
+  for (const match of commentBody.matchAll(regex)) {
+    const tag = match[0];
+    const url = match[1] || match[2];
+
+    // to avoid downloading the same file
+    if (!cache.has(url)) {
+      cache.set(url, await downloadFile(url));
+
+      files.push({
+        name: generateFileHash(url),
+        buffer: cache.get(url)
+      });
+    }
+    else if (process.env.DRY_RUN === 'true') {
+      console.info(`downloading url ${url} has skipped because the cache has found.\n`);
+    }
+
+    commentBody = commentBody.replace(new RegExp(escapeRegExp(tag) + '(\\r\\n|\\r|\\n)*'), '');
+  }
+
+  const resolvedReplacements = await Promise.all(
+    [...cache.entries()].map(async ([url, promise]) => [url, await promise])
+  );
+
+  for (const [url, newUrl] of resolvedReplacements) {
+    cache.set(url, newUrl);
+  }
+
+  return {
+    body: commentBody,
+    files: files
+  };
+}
+
+// https://chatgpt.com/share/67a6fe0a-c510-8004-9ed8-7b106493bb4a
+async function downloadFile(url) {
+  let headers = null
+  const token = process.env.PERSONAL_ACCESS_TOKEN ?
+                process.env[process.env.PERSONAL_ACCESS_TOKEN] :
+                process.env.GITHUB_TOKEN
+
+  // to avoid exposing the GitHub token to somewhere else
+  if (url.startsWith('https://github.com')) {
+    headers = {
+      'Authorization': `Bearer ${token}`,
+      'User-Agent': 'Node.js'
+    }
+  }
+  else {
+    headers = {
+      'User-Agent': 'Node.js'
+    }
+  }
+
+  if (process.env.DRY_RUN === 'true') console.info(`downloading file ${url}`)
+  const response = await fetch(url, { headers: headers })
+  if (!response.ok) throw new Error(`Failed to fetch attached file ${url}: ${response.statusText}`)
+  if (process.env.DRY_RUN === 'true') console.info(`file ${url} downloaded`)
+  if (process.env.DRY_RUN === 'true') console.info(`creating buffer from ${url}`)
+  const buffer = await response.arrayBuffer()
+  if (process.env.DRY_RUN === 'true') console.info(`buffer from ${url} created\n`)
+  return buffer;
+}
+
+// https://chatgpt.com/share/67a6fe0a-c510-8004-9ed8-7b106493bb4a
+function generateFileHash(url) {
+  return crypto.createHash('sha256').update(url, 'utf8').digest('hex').slice(0, 32);
+}
+
+function escapeRegExp(string) {
+  return string.replace(/([\x00-\x2F\x3A-\x40\x5B-\x60\x7B-\x7F])/g, '\\$&');
 }
 
 run().catch((error) => {
